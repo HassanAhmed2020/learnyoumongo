0d36189ea0732926ad19b8570ccf4f82d928523b
Hassan Ahmed <somii@hotmail.com>
2017-10-12 19:52:54 +0000

    Added FindProj Solution

--- mongoFindProj.js
+++ mongoFindProj.js
@@ -3,7 +3,8 @@
     var url = 'mongodb://localhost:27017/learnyoumongo';
     var givenAge = parseInt(process.argv[2]);
     
-    var query = { age: {$gt: givenAge} };
+    var query = [{age: {$gt: givenAge}}, {name: 1, age: 1, _id: 0 }];
+ 
     
 
 
@@ -15,9 +16,8 @@
     mongo.connect(url, function(err, db) 
                                         {
                                             // db gives access to the database
-                                            if (err) throw "Bad Bad Error Happened";
                                             var parrots = db.collection('parrots');
-                                            parrots.find(query).toArray(function(err, documents) 
+                                            parrots.find(query[0],query[1]).toArray(function(err, documents) 
 
                                                                                   {
 //                                                                                 console.log("Find it!!!!")
@@ -33,7 +33,4 @@
                                             
                                         }
                 );
-
-
-
 
