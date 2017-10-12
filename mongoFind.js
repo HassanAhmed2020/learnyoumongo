7c88943744022eabd8b9920da66b337e6a9088d4
Hassan Ahmed <somii@hotmail.com>
2017-10-12 17:31:52 +0000

    10/12 update

--- mongoFind.js
+++ mongoFind.js
@@ -3,15 +3,66 @@
     var url = 'mongodb://localhost:27017/learnyoumongo';
     var age = parseInt(process.argv[2]);
     
+    var query = { age: 10 };
+    
+
+
+
+
+
+
 
     mongo.connect(url, function(err, db) 
                                         {
                                             // db gives access to the database
                                             var parrots = db.collection('parrots');
 //                                            console.log(parrots);
-                                            db.close();
+//                                            console.log(process.argv[2]);
+                                            
+//                                            console.log(parrots.find(query));
+                                            
+//                                            parrots.find(  { age: 10} ).toArray(function(err, documents) 
+                                            parrots.find(  { age: { $gt:3} } ).toArray(function(err, documents) 
+                                                                                                            {
+//                                                                                                                console.log("Find it!!!!")
+                                                                                                                console.log(documents);
+                                                                                                                db.close();
+                                                                                                            }
+                                                                                        );
+
+                                            
+                                            
+                                            
+                                            
+                                            
                                         }
                 );
 
+
+
+
+
+
+
+
+/*
+
+
+    mongo.connect(url, function(err, db) {
+//                                            if(err) throw err;
+                                            var parrots = db.collection('parrots');
+                                            console.log(parrots);
+                                            parrots.find({
+                                                            age: { $gt: +process.argv[2] }
+                                                        }).toArray(function(err, docs)
+                                                                                        {
+                                                                                        if(err) throw err;
+                                                                                        console.log(docs);
+                                                                                        db.close();
+                                                                                        });
+        
+
+                                        }   
+                );
     
-    
+*/    
